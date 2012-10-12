bbe361571a7eca980522361aca63e2785975345d
Googol Lee <googollee@gmail.com>
2012-10-12 23:20:54 +0800

    add timeout

--- session.php
+++ session.php
@@ -17,6 +17,7 @@ function proxy() {
   curl_setopt($ch, CURLOPT_COOKIE, $cookie);
   curl_setopt($ch, CURLOPT_HEADER, 1);
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+  curl_setopt($ch, CURLOPT_TIMEOUT, 5);
 
   $schema = "http://";
   if (isset($_SERVER["HTTPS"]) && ($_SERVER["HTTPS"] == "on")) {
